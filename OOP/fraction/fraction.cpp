7a6ebb88da27a3333472830f9386c801b86f0b41
LoveSy <631499712@qq.com>
2017-03-23 12:34:06 +0800

    Add Fraction project

---
+++ OOP/fraction/fraction.cpp
@@ -0,0 +1,6 @@
+#include "fraction.h"
+
+Fraction::Fraction()
+{
+
+}
---
+++ OOP/fraction/fraction.h
@@ -0,0 +1,11 @@
+#ifndef FRACTION_H
+#define FRACTION_H
+
+
+class Fraction
+{
+public:
+    Fraction();
+};
+
+#endif // FRACTION_H
