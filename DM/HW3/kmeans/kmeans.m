c4d7625c41b4dd14a203975ff2dc841ccda892a3
yujincheng <shana@zju.edu.cn>
2018-06-15 21:11:35 +0800

    Update

--- DM/HW3/kmeans/kmeans.m
+++ DM/HW3/kmeans/kmeans.m
@@ -8,7 +8,19 @@ function [idx, ctrs, iter_ctrs] = kmeans(X, K)
 %           ctrs - cluster centers, K-by-p matrix.
 %           iter_ctrs - cluster centers of each iteration, K-by-p-by-iter
 %                       3D matrix.
-
-% YOUR CODE HERE
-
+ctrs = datasample(X, K, 1, 'Replace', false);
+iter_ctrs = ctrs;
+idx = zeros(1, size(X,1));
+while(true)
+    [~, idx_new] = pdist2(ctrs, X, 'euclidean','Smallest', 1);
+    if(idx_new == idx)
+        break;
+    end
+    for i=1:K
+        ctrs(i,:) = mean(X(idx_new == i, :));
+    end
+    iter_ctrs = cat(3, iter_ctrs, ctrs);
+    idx = idx_new;
+end
+kmeans_plot(X, idx, ctrs, iter_ctrs);
 end
